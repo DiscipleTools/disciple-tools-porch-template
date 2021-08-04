b35ee7fc9e0bbb4019040305dbbf0cc7c5cc99f5
Chris Chasm <chris@chasm.solutions>
2021-08-04 15:12:22 -0600

    Moved hardwired to a branch

--- home-hardwired/footer.php
+++
@@ -1,2 +0,0 @@
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/plugins.js"></script>
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/main.js"></script>
