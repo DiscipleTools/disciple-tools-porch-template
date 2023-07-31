ed652378bf4196c7ce82bd900568136a0e99e6aa
corsacca <corsacca@gmail.com>
2023-07-31 16:53:29 +0100

    Fix permissions

--- landing-pages/roles-and-permissions.php
+++ landing-pages/roles-and-permissions.php
@@ -36,7 +36,7 @@ class DT_Porch_Template_Landing_Roles
             'edit_'.$this->post_type => true,
             'read_'.$this->post_type => true,
             'delete_'.$this->post_type => true,
-            'delete_others_'.$this->post_type.'s',
+            'delete_others_'.$this->post_type.'s' => true,
             'delete_'.$this->post_type.'s' => true,
             'edit'.$this->post_type.'s' => true,
             'edit_others_'.$this->post_type.'s' => true,
